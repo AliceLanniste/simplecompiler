2917e7a61c8769d02184373554337aab6e7450cc
AliceLanniste <1399789151@qq.com>
2019-03-16 21:56:38 +0800

    SOME CLEAN

--- mc/Program.cs
+++ mc/Program.cs
@@ -15,27 +15,43 @@ namespace Minsk
         {
             bool showTree = false;
             var _variables = new Dictionary<VariableSymbol, object>();
+            var textBuilder = new StringBuilder();
 
             while (true)
-            {
+            {   
+                if (textBuilder.Length == 0)
+                {
                 Console.Write("> ");
-                var line = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(line))
-                    return;
-
-                if (line == "#showTree")
-                {
-                    showTree = !showTree;
-                    Console.WriteLine(showTree ? "Showing parse trees." : "Not showing parse trees");
-                    continue;
                 }
-                else if (line == "#cls")
+                var input = Console.ReadLine();
+                var isBlank = string.IsNullOrWhiteSpace(input);
+
+                if (textBuilder.Length == 0)
                 {
-                    Console.Clear();
-                    continue;
+                    if (isBlank)
+                    {
+                        break;
+                    }
+                    else if (line == "#showTree")
+                    {
+                        showTree = !showTree;
+                        Console.WriteLine(showTree ? "Showing parse trees." : "Not showing parse trees");
+                        continue;
+                    }
+                    else if (line == "#cls")
+                    {
+                        Console.Clear();
+                        continue;
+                    }
                 }
 
-                var syntaxTree = SyntaxTree.Parse(line);
+                textBuilder.Append(input);
+                var text = textBuilder.toString();
+                var syntaxTree = SyntaxTree.Parse(text);
+                
+                if (!isBlank && syntaxTree.Diagnostics.Any())
+                    continue;
+
                 var comp = new Compilation(syntaxTree);
                 var result = comp.evaluate(_variables);
 
@@ -55,15 +71,28 @@ namespace Minsk
                 {
                     foreach (var diagnostic in result.Diagnostics)
                     {
+                        var lineIndex = syntaxTree.Text.GetLineIndex(diagnostic.Span.Start);
+                        var line = syntaxTree.Text.Lines[lineIndex];
+                        var lineNumber = lineIndex + 1;
+                        var character = diagnostic.Span.Start - line.Start + 1;
+
+
                         Console.WriteLine();
 
                         Console.ForegroundColor = ConsoleColor.DarkRed;
                         Console.WriteLine(diagnostic);
                         Console.ResetColor();
 
-                        var prefix = line.Substring(0, diagnostic.Span.Start);
-                        var error = line.Substring(diagnostic.Span.Start, diagnostic.Span.Length);
-                        var suffix = line.Substring(diagnostic.Span.End);
+                        // var prefix = line.Substring(0, diagnostic.Span.Start);
+                        // var error = line.Substring(diagnostic.Span.Start, diagnostic.Span.Length);
+                        // var suffix = line.Substring(diagnostic.Span.End);
+                        var prefixSpan = TextSpan.FromBounds(line.Start, diagnostic.Span.Start);
+                        var suffixSpan = TextSpan.FromBounds(diagnostic.Span.End, line.End);
+
+                        var prefix = syntaxTree.Text.ToString(prefixSpan);
+                        var error = syntaxTree.Text.ToString(diagnostic.Span);
+                        var suffix = syntaxTree.Text.ToString(suffixSpan);
+
 
                         Console.Write("    ");
                         Console.Write(prefix);
@@ -84,6 +113,7 @@ namespace Minsk
 
                     // Console.ResetColor();
                 }
+                textBuilder.Clear();
             }
         }
 
