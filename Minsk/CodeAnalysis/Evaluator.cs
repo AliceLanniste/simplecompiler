4b7e4c869885165003cb5d1510ee9168a3c92963
AliceLanniste <1399789151@qq.com>
2025-12-07 15:18:32 +0800

    fix: Evaluate the upper bound only once in a for loop

--- Minsk/CodeAnalysis/Evaluator.cs
+++ Minsk/CodeAnalysis/Evaluator.cs
@@ -8,7 +8,9 @@ namespace Minsk.CodeAnalysis
     {
         private readonly BoundBlockStatement _root;
         private readonly Dictionary<VariableSymbol, object> _variables;
-        private object _lastedValue;
+
+        private object _lastValue;
+
         public Evaluator(BoundBlockStatement root, Dictionary<VariableSymbol, object> variables)
         {
             _root = root;
@@ -17,10 +19,9 @@ namespace Minsk.CodeAnalysis
 
         public object Evaluate()
         {
-              var labelToIndex = new Dictionary<LabelSymbol, int>();
+            var labelToIndex = new Dictionary<LabelSymbol, int>();
 
             for (var i = 0; i < _root.Statements.Length; i++)
-
             {
                 if (_root.Statements[i] is BoundLabelStatement l)
                     labelToIndex.Add(l.Label, i + 1);
@@ -49,8 +50,7 @@ namespace Minsk.CodeAnalysis
                     case BoundNodeKind.ConditionalGotoStatement:
                         var cgs = (BoundConditionalGotoStatement)s;
                         var condition = (bool)EvaluateExpression(cgs.Condition);
-                        if (condition && !cgs.JumpIfFalse ||
-                            !condition && cgs.JumpIfFalse)
+                        if (condition == cgs.JumpIfTrue)
                             index = labelToIndex[cgs.Label];
                         else
                             index++;
@@ -64,20 +64,19 @@ namespace Minsk.CodeAnalysis
 
             }
 
-            return _lastedValue;
+            return _lastValue;
         }
-     
 
         private void EvaluateVariableDeclaration(BoundVariableDeclaration node)
         {
             var value = EvaluateExpression(node.Initializer);
-            _variables.Add(node.Variable, value);
-            _lastedValue = value;
+            _variables[node.Variable] = value;
+            _lastValue = value;
         }
 
         private void EvaluateExpressionStatement(BoundExpressionStatement node)
         {
-            _lastedValue = EvaluateExpression(node.Expression);
+            _lastValue = EvaluateExpression(node.Expression);
         }
 
         private object EvaluateExpression(BoundExpression node)
@@ -87,7 +86,7 @@ namespace Minsk.CodeAnalysis
                 case BoundNodeKind.LiteralExpression:
                     return EvaluateLiteralExpression((BoundLiteralExpression)node);
                 case BoundNodeKind.VariableExpression:
-                    return this.EvaluateVariableExpression((BoundVariableExpression)node);
+                    return EvaluateVariableExpression((BoundVariableExpression)node);
                 case BoundNodeKind.AssignmentExpression:
                     return EvaluateAssignmentExpression((BoundAssignmentExpression)node);
                 case BoundNodeKind.UnaryExpression:
@@ -106,7 +105,7 @@ namespace Minsk.CodeAnalysis
 
         private object EvaluateVariableExpression(BoundVariableExpression v)
         {
-            return _variables[v.Variable];  
+            return _variables[v.Variable];
         }
 
         private object EvaluateAssignmentExpression(BoundAssignmentExpression a)
@@ -150,6 +149,21 @@ namespace Minsk.CodeAnalysis
                     return (int)left * (int)right;
                 case BoundBinaryOperatorKind.Division:
                     return (int)left / (int)right;
+                case BoundBinaryOperatorKind.BitwiseAnd:
+                    if (b.Type == (typeof(int)))
+                        return (int)left & (int)right;
+                    else
+                        return (bool)left & (bool)right;
+                case BoundBinaryOperatorKind.BitwiseOr:
+                    if (b.Type == (typeof(int)))
+                        return (int)left | (int)right;
+                    else
+                        return (bool)left | (bool)right;
+                case BoundBinaryOperatorKind.BitwiseXor:
+                    if (b.Type == (typeof(int)))
+                        return (int)left ^ (int)right;
+                    else
+                        return (bool)left ^ (bool)right;
                 case BoundBinaryOperatorKind.LogicalAnd:
                     return (bool)left && (bool)right;
                 case BoundBinaryOperatorKind.LogicalOr:
@@ -158,7 +172,7 @@ namespace Minsk.CodeAnalysis
                     return Equals(left, right);
                 case BoundBinaryOperatorKind.NotEquals:
                     return !Equals(left, right);
-                  case BoundBinaryOperatorKind.Less:
+                case BoundBinaryOperatorKind.Less:
                     return (int)left < (int)right;
                 case BoundBinaryOperatorKind.LessOrEquals:
                     return (int)left <= (int)right;
@@ -166,25 +180,9 @@ namespace Minsk.CodeAnalysis
                     return (int)left > (int)right;
                 case BoundBinaryOperatorKind.GreaterOrEquals:
                     return (int)left >= (int)right;
-                case BoundBinaryOperatorKind.BitwiseAnd:
-                  if(b.Type ==typeof(int)) 
-                    return (int)left & (int)right;
-                  else 
-                    return (bool)left & (bool)right;
-                case BoundBinaryOperatorKind.BitwiseOr:
-                     if(b.Type ==typeof(int)) 
-                    return (int)left | (int)right;
-                  else 
-                    return (bool)left | (bool)right;
-                case BoundBinaryOperatorKind.BitwiseXor:
-                      if(b.Type ==typeof(int)) 
-                    return (int)left ^ (int)right;
-                  else 
-                    return (bool)left ^ (bool)right;
                 default:
                     throw new Exception($"Unexpected binary operator {b.Op}");
             }
         }
     }
 }
-
